dcf4c567c180162d015813597be7c3e29ee2155e
Frédéric LAM <lamf@iarc.fr>
2023-03-26 21:51:54 +0200

    Set up production with postgreql

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_CLIENT', 'postgres');
 
   const connections = {
     mysql: {
@@ -44,6 +44,17 @@ module.exports = ({ env }) => {
             'DATABASE_SSL_REJECT_UNAUTHORIZED',
             true
           ),
+        },
+        acquireConnectionTimeout: 1000000,
+        pool: {
+          min: 0,
+          max: 4,
+          acquireTimeoutMillis: 300000,
+          createTimeoutMillis: 300000,
+          destroyTimeoutMillis: 300000,
+          idleTimeoutMillis: 30000,
+          reapIntervalMillis:1000,
+          createRetryIntervalMillis: 2000
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
